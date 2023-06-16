035536218c0efc8a47a8393ae08f905dd0fab25e
m najafzadeh <najafzadehm1991@gmail.com>
2023-06-17 00:39:51 +0330

    sent data to html

--- blog/models.py
+++ blog/models.py
@@ -16,12 +16,15 @@ def validate_file_extention(value):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.FileField(upload_to='files/user_avatar', null=False, blank=False, validators=[validate_file_extention])
+    avatar = models.FileField(upload_to='user_avatar', null=False, blank=False, validators=[validate_file_extention])
     description = models.CharField(max_length=512, null=False, blank=False)
+
+    def __str__(self):
+        return self.user.first_name + " " + self.user.last_name
 
 class Article(models.Model):
     title = models.CharField(max_length=128, null=False, blank=False)
-    cover = models.FileField(upload_to='files/article_cover/', null=False, blank=False, validators=[validate_file_extention])
+    cover = models.FileField(upload_to='article_cover/', null=False, blank=False, validators=[validate_file_extention])
     context = RichTextField()
     created_at = models.DateField(default=datetime.now(), blank=False)
     category = models.ForeignKey('category', on_delete=models.CASCADE)
@@ -29,4 +32,7 @@ class Article(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=128, null=False, blank=False)
-    cover = models.FileField(upload_to='files/category_cover/', null=False, blank=False, validators=[validate_file_extention])
+    cover = models.FileField(upload_to='category_cover/', null=False, blank=False, validators=[validate_file_extention])
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import *
 
 class IndexPage(TemplateView):
-    template_name = "index.html"
+
+    def get(self, request, **kwargs):
+
+        article_data = []
+        all_articles = Article.objects.all().order_by('-created_at')[:2]
+
+        for article in all_articles:
+            article_data.append({
+                'title' : article.title,
+                'cover' : article.cover.url,
+                'category' : article.category.title,
+                'created_at' : article.created_at,
+            })
+
+        context = {
+            'article_data' : article_data,
+        }
+
+        return render(request, 'index.html', context)
